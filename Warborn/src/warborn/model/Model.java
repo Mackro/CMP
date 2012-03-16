227fcf62b018ee6f0e9e4bf7c88717c454ba2c91
Mackro Otterberg <spamiuso@gmail.com>
2012-03-16 15:04:59 +0100

    testing cause git-problems

--- Warborn/src/warborn/model/Model.java
+++ Warborn/src/warborn/model/Model.java
@@ -1,20 +1,48 @@
 package warborn.model;
 
+import java.awt.Color;
+import java.util.Observable;
+
 import warborn.map.IMap;
 
-public class Model {
+public class Model extends Observable{
 	
 	private Player[] players;
 	private Territory[] territories;
+	private IMap[] maps;
+	private int selectedMap;
 	
-	public Model (Player[] players, Territory[] territories){
-		this.players = players;
-		this.territories = territories;
+	public Model (){
+		this.players = new Player[2];
+		this.territories = null;
 	}
 	
-	
+
 	public Player[] getPlayers(){
 		return players;
+	}
+	
+	public Territory[] getTerritories(){
+		return territories;
+	}
+	
+	public String[] getMapNames(){
+		String[] names = new String[maps.length];
+		for(int i = 0; i < maps.length; i++){
+			names[i] = maps[i].toString();
+		}
+		return names;
+	}
+	
+	public void setPlayers(String[] names, Color[] colours){
+		players = new Player[names.length];
+		for(int i = 0; i < names.length; i++){
+			players[i] = new Player(names[i], i, colours[i]);
+		}
+	}
+	
+	public void setSelectedMap(int id){
+		this.selectedMap = id;
 	}
 	
 	public boolean attackCompatible(Territory t1, Territory t2){
