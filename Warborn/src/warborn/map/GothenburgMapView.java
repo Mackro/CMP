33bf2ca21c03cd58a622822ae149af4b492c7713
Emil ahsberg <lovet1991@hotmail.com>
2012-03-23 08:34:08 +0100

    modified Gothenburg map so that it gets the screensize from the model

--- Warborn/src/warborn/map/GothenburgMapView.java
+++ Warborn/src/warborn/map/GothenburgMapView.java
@@ -7,21 +7,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import warborn.model.Model;
+
 public class GothenburgMapView extends JPanel implements IMap { 	
 
 	/**
 	 * Create the panel.
 	 */
-	public GothenburgMapView() {
+	public GothenburgMapView(Model model) {
 		setLayout(null);
 		
 		setSize(1366, 768);
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel.setBounds(0, 0, 1366, 768);
+		lblNewLabel.setBounds(0, 0, model.getWidth(), model.getHeight());
 		
 		Image I = new ImageIcon("images/Gothenburg.jpg").getImage();
-		I = I.getScaledInstance(TestGothenburg.getWidth(), (int) ((TestGothenburg.getHeight())*0.75), 0);
+		I = I.getScaledInstance(model.getWidth(), (int) ((model.getHeight())*0.75), 0);
 		
 		lblNewLabel.setIcon(new ImageIcon(I));
 		add(lblNewLabel);
