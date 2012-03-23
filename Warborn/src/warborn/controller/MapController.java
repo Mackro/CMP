8834df9de0a9fa9246b7f6afb8986c9a75af559e
Emil ahsberg <lovet1991@hotmail.com>
2012-03-23 16:23:42 +0100

    added Meeting 23 mars and addded map controller(not colmplete)

---
+++ Warborn/src/warborn/controller/MapController.java
@@ -0,0 +1,14 @@
+package warborn.controller;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MapController implements Observer {
+
+	@Override
+	public void update(Observable gothenburgMapView, Object e) {
+		
+		
+	}
+
+}
--- Warborn/src/warborn/map/GothenburgMapView.java
+++ Warborn/src/warborn/map/GothenburgMapView.java
@@ -1,6 +1,9 @@
 package warborn.map;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -10,15 +13,17 @@ import javax.swing.SwingConstants;
 
 import warborn.model.Model;
 
-public class GothenburgMapView extends JPanel implements IMap { 	
+public class GothenburgMapView extends Observable implements IMap, ActionListener { 	
 
 	/**
 	 * Create the Map panel.
 	 */
 	public GothenburgMapView(Model model) {
-		setLayout(null);
 		
-		setSize(model.getWidth(), model.getHeight());
+		JPanel mapPanel = new JPanel();
+		mapPanel.setLayout(null);
+		
+		mapPanel.setSize(model.getWidth(), model.getHeight());
 		JLabel maplbl = new JLabel("");
 		maplbl.setVerticalAlignment(SwingConstants.TOP);
 		maplbl.setBounds(0, 0, model.getWidth(), model.getHeight());
@@ -28,24 +33,32 @@ public class GothenburgMapView extends JPanel implements IMap {
 		I = I.getScaledInstance(model.getWidth(), (int) ((model.getHeight())*0.75), 0);
 		
 		maplbl.setIcon(new ImageIcon(I));
-		add(maplbl);
+		mapPanel.add(maplbl);
 		
 		JButton kingsslopebtn = new JButton();
-		kingsslopebtn.setBounds((int)(model.getWidth()*0.20), (int)(model.getHeight()*0.28), 40,40);
+		kingsslopebtn.setBounds((int)(model.getWidth()*0.20), (int)(model.getHeight()*0.28), 45,45);
 		kingsslopebtn.setText(model.getTerritory(21).getNbrOfUnits()+"");
+		kingsslopebtn.addActionListener(this);
 		
 		JButton cabbageNestbtn = new JButton();
-		cabbageNestbtn.setBounds((int)(model.getWidth()*0.24), (int)(model.getHeight()*0.25), 40,40);
-		cabbageNestbtn.setLabel(model.getTerritory(20).getNbrOfUnits()+"");
+		cabbageNestbtn.setBounds((int)(model.getWidth()*0.24), (int)(model.getHeight()*0.25), 45,45);
+		cabbageNestbtn.setText(model.getTerritory(20).getNbrOfUnits()+"");
+		cabbageNestbtn.addActionListener(this);
 		
 		System.out.println(model.getTerritory(20).getNbrOfUnits()+"");
 		
-		add(kingsslopebtn);
-		add(cabbageNestbtn);
+		mapPanel.add(kingsslopebtn);
+		mapPanel.add(cabbageNestbtn);
 		
 	}
 	
 	public String toString(){
 		return "Gothenburg";
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		notifyObservers(e);
+		
 	}
 }
