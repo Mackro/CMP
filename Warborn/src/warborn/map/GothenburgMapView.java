7c30818ac9eb6600a59e37a88e1cb8e39cfab079
Emil ahsberg <lovet1991@hotmail.com>
2012-03-23 11:39:46 +0100

    added a few comments on the Gothenburg maps code (need to fetch)

--- Warborn/src/warborn/map/GothenburgMapView.java
+++ Warborn/src/warborn/map/GothenburgMapView.java
@@ -12,21 +12,22 @@ import warborn.model.Model;
 public class GothenburgMapView extends JPanel implements IMap { 	
 
 	/**
-	 * Create the panel.
+	 * Create the Map panel.
 	 */
 	public GothenburgMapView(Model model) {
 		setLayout(null);
 		
-		setSize(1366, 768);
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel.setBounds(0, 0, model.getWidth(), model.getHeight());
+		setSize(model.getWidth(), model.getHeight());
+		JLabel maplbl = new JLabel("");
+		maplbl.setVerticalAlignment(SwingConstants.TOP);
+		maplbl.setBounds(0, 0, model.getWidth(), model.getHeight());
 		
+		//Scaling the Map to fit the screensize
 		Image I = new ImageIcon("images/Gothenburg.jpg").getImage();
 		I = I.getScaledInstance(model.getWidth(), (int) ((model.getHeight())*0.75), 0);
 		
-		lblNewLabel.setIcon(new ImageIcon(I));
-		add(lblNewLabel);
+		maplbl.setIcon(new ImageIcon(I));
+		add(maplbl);
 		
 	}
 	
