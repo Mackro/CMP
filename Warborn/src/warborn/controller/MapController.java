f21de7d67240e889807984e8b5a8d62614aad04d
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-03-27 08:26:40 +0200

    Changed a row in MapController

--- Warborn/src/warborn/controller/MapController.java
+++ Warborn/src/warborn/controller/MapController.java
@@ -6,7 +6,7 @@ import java.util.Observer;
 public class MapController implements Observer {
 
 	@Override
-	public void update(Observable gothenburgMapView, Object e) {
+	public void update(Observable mapView, Object e) {
 		
 		
 	}
