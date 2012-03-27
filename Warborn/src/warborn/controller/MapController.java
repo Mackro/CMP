45ff217334b7989960e74451b092e333a3467bb5
Emil ahsberg <lovet1991@hotmail.com>
2012-03-27 08:50:03 +0200

    implemented MapController class

--- Warborn/src/warborn/controller/MapController.java
+++ Warborn/src/warborn/controller/MapController.java
@@ -3,12 +3,22 @@ package warborn.controller;
 import java.util.Observable;
 import java.util.Observer;
 
+import warborn.map.IMap;
+import warborn.model.Warborn;
+
 public class MapController implements Observer {
+	
+	public MapController(Warborn model){
+		
+	}
 
 	@Override
 	public void update(Observable mapView, Object e) {
 		
-		
+		if (!(mapView instanceof IMap)){
+			return;
+		}
+		model.setSelectedTerritory((Integer)e);
 	}
 
 }
