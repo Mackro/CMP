0833ffec452d8f9ff962c4e86673eff83aa5f3bb
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-03-28 14:06:58 +0200

    Added a JUnit test file, not yet implemented

---
+++ Warborn/jUnitTest/warborn/move/MoveTest.java
@@ -0,0 +1,20 @@
+package warborn.move;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveTest {
+
+	@Test
+	public void testMove() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveUnits() {
+		fail("Not yet implemented");
+	}
+
+}
