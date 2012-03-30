9447928c766a1c89edd46b917ae323d8029e0ff6
Mackro Otterberg <spamiuso@gmail.com>
2012-03-30 11:42:24 +0200

    Started a playerTest class

---
+++ Warborn/jUnitTest/warborn/model/PlayerTest.java
@@ -0,0 +1,59 @@
+package warborn.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetNbrOfTerritories() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddTerritory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveTerritory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddCard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveCard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCanExchangeCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
