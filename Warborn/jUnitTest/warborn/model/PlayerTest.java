9efc5642c90e04fb333a92b9eef45a10f3dd1e26
Mackro Otterberg <spamiuso@gmail.com>
2012-03-30 13:07:36 +0200

    uppload PlayerTest because Project leader want's it

--- Warborn/jUnitTest/warborn/model/PlayerTest.java
+++ Warborn/jUnitTest/warborn/model/PlayerTest.java
@@ -2,23 +2,31 @@ package warborn.model;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
 public class PlayerTest {
 
 	@Test
 	public void testPlayer() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testGetID() {
-		fail("Not yet implemented"); // TODO
+		Color c = Color.BLACK;
+		String name = "name";
+		int i = 5;
+		Player test = new Player(name, i, c);
+		c = Color.YELLOW;
+		name = "wrong";
+		i = 7;
+		assertTrue(test.getColor().equals(Color.BLACK));
+		assertTrue(test.getID() == 5);
+		assertTrue(test.getName().equalsIgnoreCase("name"));
+		
 	}
 
 	@Test
 	public void testGetNbrOfTerritories() {
-		fail("Not yet implemented"); // TODO
+		Player test = new Player("name", 7, Color.BLUE);
+		test.addTerritory(new Territory("Tname", 1, ))
 	}
 
 	@Test
