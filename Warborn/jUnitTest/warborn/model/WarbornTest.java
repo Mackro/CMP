b805a4eac4a78d5730449c88be111bbc4998ef07
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-04-03 14:24:10 +0200

    More tests implemented in WarbornTest

--- Warborn/jUnitTest/warborn/model/WarbornTest.java
+++ Warborn/jUnitTest/warborn/model/WarbornTest.java
@@ -2,13 +2,44 @@ package warborn.model;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.awt.Color;
+
+import org.junit.*;
 
 public class WarbornTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Warborn model;
+	
+	@Before
+	public void Before() {
+		model = new Warborn();		
 	}
-
+	@Test
+	public void TestSetPlayers() {
+		String[] names = {"player1", "player2"};
+		Color[] colors = {Color.red, Color.blue};
+		model.setPlayers(names, colors);
+		Player[] players = model.getPlayers();
+		assertTrue(players[1].getName().equals(names[1]));
+		assertTrue(players[2].getName().equals(names[2]));
+		assertTrue(players[1].getColor().equals(colors[1]));
+		assertTrue(players[2].getColor().equals(colors[2]));
+	}
+	@Test
+	public void TestSetSelectedMap() {
+		
+	}
+	
+	
+	/**
+	 * setSelectedMap
+	 * setSelectedTerritory
+	 * addPlayer
+	 * nextState
+	 * nextPhase
+	 * removePlayer()
+	 * removePlayer(int)
+	 * startGame
+	 * attackCompatible
+	 * moveCompatible
+	 */
 }
