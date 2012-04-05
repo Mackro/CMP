722c6d1ce2b8a17f0ccd00b407fcd7e8fc8f5205
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-04-06 00:58:51 +0200

    small stuff only

--- Warborn/src/warborn/controller/MapController.java
+++ Warborn/src/warborn/controller/MapController.java
@@ -2,13 +2,15 @@ package warborn.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 
 import warborn.map.IMap;
 import warborn.model.Warborn;
 
-public class MapController implements ActionListener {
+public class MapController implements ActionListener, KeyListener {
 	
 	private Warborn model;
 	//private IMap map;
@@ -27,5 +29,28 @@ public class MapController implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		model.setSelectedTerritory(Integer.parseInt(((JButton)e.getSource()).getActionCommand() + ""));
 	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent evt) {
+		System.out.println("Char: "+evt.getKeyChar() + "  ,Code: " + evt.getKeyCode() + "  ,Location: " + evt.getKeyLocation());
+		/*
+		if(evt.getKeyCode() == KeyEvent.VK_ESCAPE){
+			System.out.println("Det funkar");
+		} else {
+			System.out.println("funkar inte");
+		}*/
 	}
 }
