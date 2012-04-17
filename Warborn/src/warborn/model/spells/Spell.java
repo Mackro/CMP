346cbc59afb98d10b7114e7c35d7b4c57cace4d8
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-04-17 16:06:31 +0200

    added a ticker to Spell class

--- Warborn/src/warborn/model/spells/Spell.java
+++ Warborn/src/warborn/model/spells/Spell.java
@@ -5,18 +5,28 @@ import warborn.model.Warborn;
 public abstract class Spell {
 	
 	private int manaCost;
+	public int timer;
 	
 	public abstract boolean validTarget(Warborn model);
 	public abstract String getName();
 	public abstract String getDescription();
 	public abstract void invoke (Warborn model);
 	
-	public Spell(int mana){
+	public Spell(int mana, int time){
 		manaCost = mana;
+		timer = time;
 	}
 	
 	public int getManaCost(){
 		return manaCost;
+	}
+	
+	public int getTimer(){
+		return timer;
+	}
+	
+	public void tick(){
+		timer--;
 	}
 	
 	public String toString() {
